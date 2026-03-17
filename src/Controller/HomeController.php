e3c4ed9abf7752b436aae329cee68642faeadf39
Juan Gomes <juangomes376@gmail.com>
2026-03-17 17:41:46 +0100

    ADD HomeController + tailwind Css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,10 +10,23 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EventRepository $eventRepository): Response
     {
+        $today = new \DateTimeImmutable('today');
+
+        $events = $eventRepository
+            ->createQueryBuilder('e')
+            ->andWhere('e.isPublished = :published')
+            ->andWhere('e.dateDebut >= :today')
+            ->setParameter('published', true)
+            ->setParameter('today', $today)
+            ->orderBy('e.dateDebut', 'ASC')
+            ->setMaxResults(6)
+            ->getQuery()
+            ->getResult();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'events' => $events,
         ]);
     }
 }
