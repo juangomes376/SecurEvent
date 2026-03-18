c904e8a59bd2d0ddcf67d8b7b5afa01303b3d806
Juan Gomes <juangomes376@gmail.com>
2026-03-18 15:12:22 +0100

    Revert "ADD Style"
    This reverts commit d4bb07459a9fffa0738c59195e02512b5fa84e9b.

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
