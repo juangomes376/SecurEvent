43cb6469828b910a0a58b3c9c4f77d4bca98c355
Juan Gomes <juangomes376@gmail.com>
2026-03-20 15:49:30 +0100

    update categorie dan evenController

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -18,9 +18,15 @@ use Symfony\Component\Routing\Attribute\Route;
 final class EventController extends AbstractController
 {
     #[Route(name: 'app_event_index', methods: ['GET'])]
-    public function index(EventRepository $eventRepository, EventService $eventService, CategoriesService $categoriesService): Response
+    public function index(EventRepository $eventRepository, EventService $eventService, CategoriesService $categoriesService, Request $request): Response
     {
-        $events = $eventRepository->findAll();
+        $selectedCategory = $request->query->has('category') ? $request->query->get('category') : null;
+        if ($selectedCategory) {
+            $events = $eventService->findAllEventsByCategory($selectedCategory, $eventRepository);
+        } else {
+            $events = $eventRepository->findAll();
+        }
+
         foreach ($events as $event) {
             $event->reserved = $eventService->isEventReservedByUser($event, $this->getUser());
             $event->placesRestantes = $eventService->placesRestantes($event);
@@ -29,7 +35,7 @@ final class EventController extends AbstractController
         return $this->render('event/index.html.twig', [
             'events' => $events,
             'categories' => $categoriesService->getAllCategories(),
-            'selectedCategory' => null,
+            'selectedCategory' => $selectedCategory,
         ]);
     }
 
